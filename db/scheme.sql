91630e4d461036d3f8a7b406877f2eddf9b95ee9
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-04-07 16:06:23 -0400

    Changed variable name

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,8 +1,8 @@
--- Checks for existing database if its exist ,deletes the database
-DROP DATABASE IF EXISTS employeTracker_db
+g-- Checks for existing database if it exists, deletes the database
+DROP DATABASE IF EXISTS employeetracker_db;
 -- Create a new database
-CREATE DATABASE employeeTracker_db;
-\c employeeTracker_db
+CREATE DATABASE employeetracker_db;
+\c employeetracker_db;
 
 -- Create a new table department
 CREATE TABLE department (
@@ -15,17 +15,17 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title varchar(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department INT,
-    FOREIGN KEY (department) REFERENCES department(id)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 -- Create a new table employee
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name varchar(30),
-    last_name varchar(30),
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
     role_id INT NOT NULL,
-    maneger_id INT NOT NULL,
+    manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (maneger_id) REFERENCES employee(id),
-)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);

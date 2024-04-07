0516b4f8144e37f59adff8335eb9e812b0edafde
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-04-06 20:09:32 -0400

    Creating structure of server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+//DEPENDENCIES
+const express = require("express");
+
+// Import and require Pool (node-postgres)
+// We'll be creating a Connection Pool
+const { Pool } = require("pg");
+const fs = require("fs");
+
+// PORT \ APP
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const pool = new Pool(
+  {
+    user: "postgres",
+    password: "password123",
+    host: "localhost",
+    database: "",
+  },
+  console.log(`Connected to the ... database.`)
+);
+
+pool.connect();
+
+//ROUTES
+//Api Routes
+
+//START APP
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

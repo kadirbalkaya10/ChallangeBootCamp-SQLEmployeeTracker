bf3945c15f8ef3cd3fce34ae8a908913ec3a9339
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-04-06 20:28:39 -0400

    Creating Database, Creating tables for databes

---
+++ db/scheme.sql
@@ -0,0 +1,31 @@
+-- Checks for existing database if its exist ,deletes the database
+DROP DATABASE IF EXISTS employeTracker_db
+-- Create a new database
+CREATE DATABASE employeTracker_db;
+\c employeTracker_db
+
+-- Create a new table department
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name varchar(30) UNIQUE NOT NULL
+);
+
+-- Create a new table role
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title varchar(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department INT,
+    FOREIGN KEY (department) REFERENCES department(id)
+);
+
+-- Create a new table employee
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name varchar(30),
+    last_name varchar(30),
+    role_id INT NOT NULL,
+    maneger_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (maneger_id) REFERENCES employee(id),
+)
